f1f7c0055c555a613242b877e92540683cf63e93
drxcy <rachitbahuguna5@gmail.com>
2024-03-03 14:04:53 +0530

    "Adding Routes BrowserRouter to pages file for routing"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import {BrowserRouter ,Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-950'>
-      app
-    </h1>
+    <BrowserRouter className='text-3xl text-red-950'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-out' element={<SignOut />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
