4c61912e66ec67a261815816b148b88e98691e05
drxcy <rachitbahuguna5@gmail.com>
2024-03-03 17:40:08 +0530

    Header NavBar Structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import SignIn from './pages/SignIn';
 import SignOut from './pages/SignOut';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
+import Header from './components/Header';
 export default function App() {
   return (
-    <BrowserRouter className='text-3xl text-red-950'>
+    <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
