43ebd869d1e92d5febc5038a7ff682c1f2695658
drxcy <rachitbahuguna5@gmail.com>
2024-03-29 16:23:07 +0530

    create update user route to the backend

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
+import { test, upadateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 router.get("/test", test);
+router.put("/update/:userId",verifyToken,upadateUser)
 export default router;
