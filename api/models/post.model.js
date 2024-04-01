60f16b5f41a5f597ea7b4b6647d78fab0cf9c6bb
drxcy <rachitbahuguna5@gmail.com>
2024-04-01 22:33:26 +0530

    add create post api route to the backend

---
+++ api/models/post.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        image: {
+            type: String,
+            default:'https://www.travelpayouts.com/blog/wp-content/uploads/2021/02/blog-images-1536x808.png',
+        },
+        category: {
+            type:String,
+            default:'uncategorized',
+        },
+        slug: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        
+    },{timestamps:true}
+)
+const Post = mongoose.model('Post', postSchema);
+export default Post;
